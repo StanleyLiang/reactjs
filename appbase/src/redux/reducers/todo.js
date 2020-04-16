8b9ee006a8ad75cb805c7ca19da6cfb699a69dd3
stanley.liang <stanely01@gmail.com>
2020-04-15 23:22:45 -0700

    react + redux + scss sample

---
+++ appbase/src/redux/reducers/todo.js
@@ -0,0 +1,13 @@
+const initialState = {
+    tasks: [
+        'house clean',
+        'loundry service',
+        'work out'
+    ]
+};
+
+const todo = (state = initialState, action) => {
+    return state;
+};
+
+export default todo;
